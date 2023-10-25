67f3df53315e6f45b69b05c5617fe31ca07c5e1f
artificial <59352535+artificialbutter@users.noreply.github.com>
2023-10-24 17:44:01 -0700

    total defeat

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -4,6 +4,7 @@ module.exports = {
     args: true,
     usage: '<kisses|hugs|dominations|deaths>',
     async execute(message, args) {
+        return message.channel.send('i give up screw this\ngo code it yourself <https://github.com/artificialbutter/uwu-haiiii>');
         const { Member } = require('..');
 
         let membersdb = await Member.findAll();
@@ -24,23 +25,22 @@ module.exports = {
                 dbsearch = "deaths"
                 break;
             default:
-                return message.channel.send('you need to provide a valid argument!\nsee `help leaderboard');
+                return message.channel.send('you need to provide a valid argument!\nsee `help leaderboard`');
         }
 
         membersdb.sort(function (a, b) {
             return b[dbsearch] - a[dbsearch];
         });
 
-        let mostKissed = membersdb.slice(0, 4);
+        let mostKissed = membersdb.slice(0, 2);
+        console.log(mostKissed);
         
         try {
         message.channel.send(
             `**Leaderboard - ${args[0]}**
             \n1. <@${mostKissed[0].userid}> with **${mostKissed[0][dbsearch] || "0"}** ${args[0]}
             \n2. <@${mostKissed[1].userid}> with **${mostKissed[1][dbsearch] || "0"}** ${args[0]}
-            \n3. <@${mostKissed[2].userid}> with **${mostKissed[2][dbsearch] || "0"}** ${args[0]}
-            \n4. <@${mostKissed[3].userid}> with **${mostKissed[3][dbsearch] || "0"}** ${args[0]}
-            \n5. <@${mostKissed[4].userid}> with **${mostKissed[4][dbsearch] || "0"}** ${args[0]}`,
+            \n3. <@${mostKissed[2].userid}> with **${mostKissed[2][dbsearch] || "0"}** ${args[0]}`,
             { allowedMentions: { parse: [] } });
         } catch (error) {
             console.error(error)
