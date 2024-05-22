679fa56dddb858bde8dca3f1f09a98ab5801a8fe
Arti <59352535+artifishvr@users.noreply.github.com>
2024-05-21 19:08:22 -0700

    fresh start

--- commands/mostkissed.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'mostkissed',
-    description: 'leaderboard of most kissed people (global top 5)',
-    async execute(message) {
-        message.channel.send(`Replaced by \`leaderboard kisses\`.`);
-    },
-};
